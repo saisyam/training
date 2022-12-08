0c11506f601943f6a249d05c83d70130b82283ba
Rushika Shah <rushika.shah@anblicks.com>
2022-12-08 18:19:14 +0530

    adding repo columns

--- top_repos.py
+++ top_repos.py
@@ -12,6 +12,7 @@ for i in range(1,6):
     if resp.status_code == 200:
         data = resp.json()
         items = data['items']
+        
         for item in items:
             if item['license'] is not None and item['license']['key']is not None:
                 license = License(
@@ -23,11 +24,12 @@ for i in range(1,6):
                 )
                 db_session.add(license)
                 db_session.commit()
-                # print(license)
             else:
                 license=License()
                 db_session.add(license)
                 db_session.commit()
+
+
             if item['owner']['id'] not in owner_id_Unique_list:
                 owner_id_Unique_list.append(item['owner']['id'])
                 if item['owner']['id'] in owner_id_Unique_list and not None:
@@ -53,8 +55,6 @@ for i in range(1,6):
                     )
                     db_session.add(owner)
                     db_session.commit()
-                    # print(owner)
-             
             else:
                 owner_id_duplicate_list.append(item['owner']['id'])
                 db_session.commit()
@@ -62,15 +62,85 @@ for i in range(1,6):
             
             repository = Repository(
                 id = item['id'],
-                name = item['name'],
                 node_id = item['node_id'],
+                name = item['name'],
                 full_name = item['full_name'],
                 private = item['private'],
+                html_url = item['html_url'],
+                description = item['description'],
+                fork = item['fork'],
+                url = item['url'],
+                forks_url = item['forks_url'],
+                keys_url = item['keys_url'],
+                collaborators_url = item['collaborators_url'],
+                teams_url = item['teams_url'],
+                hooks_url = item['hooks_url'],
+                issue_events_url = item['issue_events_url'],
+                events_url = item['events_url'],
+                assignees_url = item['assignees_url'],
+                branches_url = item['branches_url'],
+                tags_url = item['tags_url'],
+                blobs_url = item['blobs_url'],
+                git_tags_url = item['git_tags_url'],
+                git_refs_url = item['git_refs_url'],
+                trees_url = item['trees_url'],
+                statuses_url = item['statuses_url'],
+                languages_url = item['languages_url'],
+                stargazers_url = item['stargazers_url'],
+                contributors_url = item['contributors_url'],
+                subscribers_url = item['subscribers_url'],
+                subscription_url = item['subscription_url'],
+                commits_url = item['commits_url'],
+                git_commits_url = item['git_commits_url'],
+                comments_url = item['comments_url'],
+                issue_comment_url = item['issue_comment_url'],
+                contents_url = item['contents_url'],
+                compare_url = item['compare_url'],
+                merges_url = item['merges_url'],
+                archive_url = item['archive_url'],
+                downloads_url = item['downloads_url'],
+                issues_url = item['issues_url'],
+                pulls_url = item['pulls_url'],
+                milestones_url = item['milestones_url'],
+                notifications_url = item['notifications_url'],
+                labels_url = item['labels_url'],
+                releases_url = item['releases_url'],
+                deployments_url = item['deployments_url'],
+                created_at = item['created_at'],
+                updated_at = item['updated_at'],
+                pushed_at = item['pushed_at'],
+                git_url = item['git_url'],
+                ssh_url = item['ssh_url'],
+                clone_url = item['clone_url'],
+                svn_url = item['svn_url'],
+                homepage = item['homepage'],
                 size = item['size'],
+                stargazers_count = item['stargazers_count'],
+                watchers_count = item['watchers_count'],
+                language = item['language'],
+                has_issues = item['has_issues'],
+                has_projects = item['has_projects'],
+                has_downloads = item['has_downloads'],
+                has_wiki = item['has_wiki'],
+                has_discussions = item['has_discussions'],
+                forks_count = item['forks_count'],
+                mirror_url = item['mirror_url'],
+                archived = item['archived'],
+                disabled = item['disabled'],
+                open_issues_count = item['open_issues_count'],
+                allow_forking = item['allow_forking'],
+                is_template = item['is_template'],
+                web_commit_signoff_required = item['web_commit_signoff_required'],
+                topics = item['topics'],
+                visibility = item['visibility'],
+                forks = item['forks'],
+                open_issues = item['open_issues'],
+                watchers = item['watchers'],
+                default_branch = item['default_branch'],
+                score = item['score'],
                 license_id = license.id,
-                owner_id=owner.id
+                owner_id = owner.id
             )
-            # print(repository)
             db_session.add(repository)
             db_session.commit()
            
