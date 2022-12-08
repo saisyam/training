6e6509ec0ef3258a3d6354d5440c9cbed4c98501
JVinay7 <118791692+JVinay7@users.noreply.github.com>
2022-12-08 18:49:37 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,17 +3,18 @@ from sqlalchemy.orm import sessionmaker
 import os
 import psycopg2
 from dotenv import load_dotenv
+from conf import *
 
 load_dotenv()
 
 def create_db_string():
     host = os.getenv("DB_HOST")
     user = os.getenv("DB_USER")
-    upass = os.getenv("DB_PASSWORD")
+    password = os.getenv("DB_PASSWORD")
     port = os.getenv("DB_PORT")
-    db = os.getenv("DB_DATABASE")
+    database = os.getenv("DB_DATABASE")
 
-    db_uri = 'postgresql://'+user+':'+upass+'@'+host+':'+port+'/'+db
+    db_uri = 'postgresql://'+user+':'+password+'@'+host+':'+port+'/'+database
     return db_uri
     
 
