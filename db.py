248c0b7f19489250e981f3631ea0ad7d2ebda771
ch-navya <118792471+ch-navya@users.noreply.github.com>
2022-12-20 18:52:10 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,21 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
+import psycopg2
+from dotenv import load_dotenv
 
 load_dotenv()
 
 def create_db_string():
-    host = os.getenv("PG_HOST")
-    user = os.getenv("PG_USER")
-    upass = os.getenv("PG_PASS")
-    port = os.getenv("PG_PORT")
-    db = os.getenv("PG_DB")
-    print(host)
-    print(user)
+    host = os.getenv("DB_HOST")
+    user = os.getenv("DB_USER")
+    upass = os.getenv("DB_PASS")
+    port = os.getenv("DB_PORT")
+    db = os.getenv("DB_DATABASE")
 
     db_uri = 'postgresql://'+user+':'+upass+'@'+host+':'+port+'/'+db
     return db_uri
+    
+
+  
 
 def create_session():
     db_uri = create_db_string()
@@ -23,6 +25,16 @@ def create_session():
     Session = sessionmaker(bind=engine)
     return Session()
 
+
+def authentication():
+    TOKEN=os.getenv('token')
+    
+    headers = {'Authorization': 'token '+TOKEN}
+    return headers
+
+
 def close_session(s):
     s.close()
-Footer
+
+
+
